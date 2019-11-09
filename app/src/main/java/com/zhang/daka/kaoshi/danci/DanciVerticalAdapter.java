ed44cf14471850d43c461d3fea79e5f64ed898f3
张运才 <zhangyc@asia5b.com>
2019-11-09 19:30:10 +0800

    优化

--- app/src/main/java/com/zhang/daka/kaoshi/danci/DanciVerticalAdapter.java
+++ app/src/main/java/com/zhang/daka/kaoshi/danci/DanciVerticalAdapter.java
@@ -34,71 +34,8 @@ public class DanciVerticalAdapter extends BaseQuickAdapter<WordInfo, BaseViewHol
         helper.setText(R.id.tv_dancivertical_worden, item.getWordEn());
         helper.setText(R.id.tv_dancivertical_wordcn, item.getWordCn());
 
-        WebView webView = helper.getView(R.id.webview_dancivertical);
-        setting(webView);
-        setWebViewChormeClient(webView);
-        webView.loadUrl(String.format("http://m.iciba.com/%s", item.getWordEn()));
-
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
-    protected WebSettings setting(WebView webView) {
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);//是否允许执行js，默认为false。设置true时，会提醒可能造成XSS漏洞
-        settings.setAllowContentAccess(true);
-        settings.setSupportMultipleWindows(true);
-        //设置缓存模式
-        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
-        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
-        settings.setJavaScriptCanOpenWindowsAutomatically(true);//设置js可以直接打开窗口，如window.open()，默认为false
-        settings.setSupportZoom(true);//是否可以缩放，默认true
-//        settings.setBuiltInZoomControls(true);//是否显示缩放按钮，默认false
-        settings.setUseWideViewPort(true);//设置此属性，可任意比例缩放。大视图模式
-        settings.setLoadWithOverviewMode(true);//和setUseWideViewPort(true)一起解决网页自适应问题
-        settings.setTextZoom(100); // 就可以禁止缩放，按照百分百显示。
-        settings.setAppCacheEnabled(false);//是否使用缓存
-        settings.setDomStorageEnabled(true);//DOM Storage
-        settings.setAllowFileAccess(true); // 允许访问文件
-        // displayWebview.getSettings().setUserAgentString("User-Agent:Android");//设置用户代理，一般不用
-        settings.setPluginState(WebSettings.PluginState.ON);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-        }
-        return settings;
-    }
-
-    protected void setWebViewChormeClient(WebView webView) {
-
-
-        webView.setWebChromeClient(new WebChromeClient() {
-
-
-        });
-        webView.setWebViewClient(new WebViewClient() {
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-//                clickYuyin(webView);
-            }
-
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                super.onPageStarted(view, url, favicon);
-            }
-
-            @Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                handler.proceed();
-            }
-
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                Timber.d("----------->>>>>>>>-----------url:" + url);
-                return super.shouldOverrideUrlLoading(view, url);
-            }
-        });
-    }
 
 
 }
--- app/src/main/java/com/zhang/daka/kaoshi/danci/DanciVerticalFragment.java
+++ app/src/main/java/com/zhang/daka/kaoshi/danci/DanciVerticalFragment.java
@@ -1,9 +1,18 @@
 package com.zhang.daka.kaoshi.danci;
 
+import android.graphics.Bitmap;
+import android.net.http.SslError;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.LinearSnapHelper;
 import android.support.v7.widget.RecyclerView;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 import android.widget.RadioGroup;
 
@@ -32,6 +41,12 @@ public class DanciVerticalFragment extends SimpleFragment {
     RecyclerView mRecyclerView;
     private Disposable subscribe;
     private ProgressBar pb_dancivertical;
+    private LinearLayoutManager linearLayoutManager;
+    private RadioGroup rg_dancivertical;
+    private WebView webView;
+    private DanciVerticalAdapter mAdapter;
+    private List<WordInfo> wordInfos;
+
 
     @Override
     public int getLayoutId() {
@@ -39,28 +54,49 @@ public class DanciVerticalFragment extends SimpleFragment {
         return R.layout.fragment_dancivertical;
     }
 
-    @Override
-    public void initEvent() {
-
-    }
 
     @Override
     public void initView() {
 
         pb_dancivertical = getView().findViewById(R.id.pb_dancivertical);
         pb_dancivertical.setMax(10);
+        rg_dancivertical = getView().findViewById(R.id.rg_dancivertical);
+        webView = getView().findViewById(R.id.webview_dancivertical);
+        setting(webView);
+        setWebViewChormeClient(webView);
 
         if (getArguments() != null) {
             position = getArguments().getInt("position", 0);
-            List<WordInfo> wordInfos = (List<WordInfo>) getArguments().getSerializable("datas");
+            wordInfos = (List<WordInfo>) getArguments().getSerializable("datas");
             mRecyclerView = getView().findViewById(R.id.rv_dancivertical);
-            DanciVerticalAdapter mAdapter = new DanciVerticalAdapter(wordInfos);
-            mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            mAdapter = new DanciVerticalAdapter(wordInfos);
+            linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+            mRecyclerView.setLayoutManager(linearLayoutManager);
             mRecyclerView.setAdapter(mAdapter);
             LinearSnapHelper snapHelper = new LinearSnapHelper();
             snapHelper.attachToRecyclerView(mRecyclerView);
         }
 
+    }
+
+    @Override
+    public void initEvent() {
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                int firstVisibleItemPosition = linearLayoutManager.findFirstVisibleItemPosition();
+                if (firstVisibleItemPosition >= 0 && wordInfos.size() > firstVisibleItemPosition) {
+                    WordInfo wordInfo = wordInfos.get(firstVisibleItemPosition);
+                    loadUrl(wordInfo);
+                }
+            }
+        });
     }
 
     @Override
@@ -74,23 +110,25 @@ public class DanciVerticalFragment extends SimpleFragment {
                 .subscribe(aLong -> {
                     Timber.d("----------->>>>>>>>-----------aLong:" + aLong);
                     if (aLong % 10 == 0 && aLong != 0) {
-                        int firstVisibleItemPosition = ((LinearLayoutManager) mRecyclerView.getLayoutManager()).findFirstVisibleItemPosition();
+                        int firstVisibleItemPosition = linearLayoutManager.findFirstVisibleItemPosition();
                         firstVisibleItemPosition++;
                         mRecyclerView.smoothScrollToPosition(firstVisibleItemPosition);
-//                        WebView webView = mRecyclerView.getChildAt(firstVisibleItemPosition).findViewById(R.id.webview_dancivertical);
-//                        RadioGroup radioGroup = mRecyclerView.getChildAt(firstVisibleItemPosition).findViewById(R.id.rg_dancivertical);
-//                        clickYuyin(webView, radioGroup);
+
+                    }
+                    if (aLong % 3 == 0) {
+                        clickYuyin();
                     }
                     pb_dancivertical.setProgress(Math.toIntExact(aLong % 10), true);
                 }, throwable -> throwable.printStackTrace());
+
     }
 
 
-    private void clickYuyin(WebView webView, RadioGroup radioGroup) {
+    private void clickYuyin() {
 
-        if (radioGroup.getCheckedRadioButtonId() == R.id.rb_dancivertical_english) {
+        if (rg_dancivertical.getCheckedRadioButtonId() == R.id.rb_dancivertical_english) {
             webView.loadUrl("javascript:document.getElementsByClassName(\"dic-sound\")[0].click();");
-        } else if (radioGroup.getCheckedRadioButtonId() == R.id.rb_dancivertical_america) {
+        } else if (rg_dancivertical.getCheckedRadioButtonId() == R.id.rb_dancivertical_america) {
             webView.loadUrl("javascript:document.getElementsByClassName(\"dic-sound\")[1].click();");
         } else {
 
@@ -131,5 +169,68 @@ public class DanciVerticalFragment extends SimpleFragment {
         super.onDestroyView();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
+    protected WebSettings setting(WebView webView) {
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);//是否允许执行js，默认为false。设置true时，会提醒可能造成XSS漏洞
+        settings.setAllowContentAccess(true);
+        settings.setSupportMultipleWindows(true);
+        //设置缓存模式
+        settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);//设置js可以直接打开窗口，如window.open()，默认为false
+        settings.setSupportZoom(true);//是否可以缩放，默认true
+//        settings.setBuiltInZoomControls(true);//是否显示缩放按钮，默认false
+        settings.setUseWideViewPort(true);//设置此属性，可任意比例缩放。大视图模式
+        settings.setLoadWithOverviewMode(true);//和setUseWideViewPort(true)一起解决网页自适应问题
+        settings.setTextZoom(100); // 就可以禁止缩放，按照百分百显示。
+        settings.setAppCacheEnabled(false);//是否使用缓存
+        settings.setDomStorageEnabled(true);//DOM Storage
+        settings.setAllowFileAccess(true); // 允许访问文件
+        // displayWebview.getSettings().setUserAgentString("User-Agent:Android");//设置用户代理，一般不用
+        settings.setPluginState(WebSettings.PluginState.ON);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+        return settings;
+    }
+
+    private void loadUrl(WordInfo item) {
+
+        webView.loadUrl(String.format("http://m.iciba.com/%s", item.getWordEn()));
+    }
+
+    protected void setWebViewChormeClient(WebView webView) {
+
+
+        webView.setWebChromeClient(new WebChromeClient() {
+
+
+        });
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+//                clickYuyin(webView);
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Timber.d("----------->>>>>>>>-----------url:" + url);
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
+    }
 
 }
